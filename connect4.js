a26c73e8fba778b43f735a17c9f9ea0a5b9148af
Richard-Harris1992 <104505323+Richard-Harris1992@users.noreply.github.com>
2022-11-09 17:57:44 -0600

    added functionality

--- connect4.js
+++ connect4.js
@@ -14,46 +14,9 @@ I need a function that will run through empty slots and see if they pick one val
 
 I think for to pass the either current state or available state it would look like
 
-atttack function player - Idk how to do here becuase the event listeners are contained in the elements.
-I could do a click, e.target if this is contained in the array turn red.
 
-maybe check the class here and then pass the value to the main array and update it.
-
-maybe .bind() alt to use a function within the connectfour class
 
 end goal is flexibility of methods needed to propery use minimax.
-
-validMove(htmlNode = null) {
-    //This is for when the player clicks an element.
-    if(htmlNode != null) {
-         if (this.gameOver) {
-                    return;
-                }
-
-
-                let coords = htmlNode.id.split('-'); //'0-0' -> [0,0]
-
-                let row = parseInt(coords[0]);
-                let col = parseInt(coords[1]);
-
-                row = this.availableMoves[col]; //pick the number at the index of col
-                if (row < 0) { //should call win-game method and call it a draw.
-                    return;
-                }
-                return this.gameBoard[row][col];
-    } //player if.
-} else
-
-    valid moves to choose from
-    validMovesComputer = []
-    for(let columnIndex = 0; columnIndex < this.currentColumns.length; columnIndex++) {
-        validMovesComputer.push(this.gameBoard[columnIndex,this.currentColumns[columnIndex]]) this pushes a coordinate of each available move to check.
-    }
-
-
-
-
-
 
 
 */
@@ -125,13 +88,23 @@ class ConnectFour {
 
     }
     //test area for minimaxx fxn
-    minimax() {
+    minimax(currentBoardState, computerColorOnBoard) {
         let currentGameState = this.currentGameState();
         let computerMarker = this.player2.color;
         console.log(currentGameState);
         console.log(computerMarker);
         let currentEmptyCells = currentGameState.filter(slots => typeof slots == 'object');
         console.log(currentEmptyCells);
+
+
+
+
+
+
+
+
+
+
     }
 
     addColorChangeClickEvent() { ///THIS MIGHT BE MY MAIN FXN inside click event. like the click event will trigger minimax and check fxn
@@ -153,20 +126,12 @@ class ConnectFour {
 
 
 
-                let playerMove = this.validMove(htmlNode);
-                if (this.currentPlayer == this.player1) {  //I may move this into a function like validMove later.
-                    playerMove.htmlElement.classList.add(this.player1.color);
-                    this.currentPlayer = this.player2;
-
-                } else {
-                    playerMove.htmlElement.classList.add(this.player2.color);
-                    this.currentPlayer = this.player1;
-
-                }
+                 this.validMove(htmlNode);
+            
 
                 //this.minimax(this.currentGameState, this.player2.color) // clean up all minimax stuff NOW!
-                console.log(this.currentGameState())
-                this.checkWinner();
+                
+               
             }); //end eventListener function
         });
 
@@ -182,13 +147,37 @@ class ConnectFour {
             let row = parseInt(coords[0]);
             let col = parseInt(coords[1]);
 
-            row = this.availableMoves[col]; //pick the number at the index of col
+            row = this.availableMoves[col]; //Places the coin in the lowest row on that column
+            
+            if(row < 0) {
+                return
+            }
 
-
-
-            let slotIndex = this.gameBoard[row][col]
-            this.currentPlayer == this.player1 ? this.gameBoard[row][col] = 'Red' : this.gameBoard[row][col] = 'Yellow';
-
+            let slotIndex = this.gameBoard[row][col];
+            
+            //change this when functionality permits as far as computer playability \/
+            if (this.currentPlayer == this.player1) {  
+                slotIndex.htmlElement.classList.add(this.player1.color);
+                this.gameBoard[row][col] = 'Red' 
+                
+                let winner = this.checkWinner(row, col);
+                if(winner) {
+                    this.setWinner(row, col)
+                }
+                this.currentPlayer = this.player2;
+            //for player2
+            } else { 
+                slotIndex.htmlElement.classList.add(this.player2.color);
+                this.gameBoard[row][col] = 'Yellow'
+                
+                let winner = this.checkWinner(row, col);
+                if(winner) {
+                    this.setWinner(row, col)
+                }
+                this.currentPlayer = this.player1;
+            }
+            
+            
             row--; //updating row height for the column
             this.availableMoves[col] = row; //update array
 
@@ -216,54 +205,51 @@ class ConnectFour {
         return this.gameBoard.flat();
     }
 
-    checkWinner() {
-        //sliding window technique
-        //horizontally
+    checkWinner(r, c) {
+        
+        //Horizontally
         for (let r = 0; r < this.rows; r++) {
             for (let c = 0; c < this.columns - 3; c++) { //-3 is to allow checking 3 ahead without going out of bounds on the array.
                 if (this.gameBoard[r][c] != ' ') {
                     if (this.gameBoard[r][c] == this.gameBoard[r][c + 1] && this.gameBoard[r][c + 1] == this.gameBoard[r][c + 2] && this.gameBoard[r][c + 2] == this.gameBoard[r][c + 3]) {
-                        this.setWinner(r, c);
-                        return;
+                        return true;
                     }
                 }
             }
         }
-        // Vertical
+        // Vertically
         for (let r = 0; r < this.rows - 3; r++) {
             for (let c = 0; c < this.columns; c++) {
                 if (this.gameBoard[r][c] != ' ') {
                     if (this.gameBoard[r][c] == this.gameBoard[r + 1][c] && this.gameBoard[r + 1][c] == this.gameBoard[r + 2][c] && this.gameBoard[r + 2][c] == this.gameBoard[r + 3][c]) {
-                        this.setWinner(r, c);
-                        return;
+                        return true;
                     }
                 }
             }
         }
 
-        //Horizontally-Left
+        //Diagonally-Left
         for (let r = 0; r < this.rows - 3; r++) {
             for (let c = 0; c < this.columns - 3; c++) {
                 if (this.gameBoard[r][c] != ' ') {
                     if (this.gameBoard[r][c] == this.gameBoard[r + 1][c + 1] && this.gameBoard[r + 1][c + 1] == this.gameBoard[r + 2][c + 2] && this.gameBoard[r + 2][c + 2] == this.gameBoard[r + 3][c + 3]) {
-                        this.setWinner(r, c);
-                        return;
+                        return true;
                     }
                 }
             }
         }
 
-        //Horizontally-Right
+        //Diagonaally-Right
         for (let r = 3; r < this.rows; r++) {
             for (let c = 0; c < this.columns - 3; c++) {
                 if (this.gameBoard[r][c] != ' ') {
                     if (this.gameBoard[r][c] == this.gameBoard[r - 1][c + 1] && this.gameBoard[r - 1][c + 1] == this.gameBoard[r - 2][c + 2] && this.gameBoard[r - 2][c + 2] == this.gameBoard[r - 3][c + 3]) {
-                        this.setWinner(r, c);
-                        return;
+                        return true;
                     }
                 }
             }
         }
+        return false;
     }
 
     setWinner(r, c) {
